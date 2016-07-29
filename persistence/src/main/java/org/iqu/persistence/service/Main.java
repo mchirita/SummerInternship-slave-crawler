7e601b562559df9cd4f4eba626133cfe5d52f719
Beniamin Savu <benisavu@gmail.com>
2016-07-29 13:31:56 +0300

    Minor modification

--- persistence/src/main/java/org/iqu/persistence/service/Main.java
+++
@@ -1,8 +0,0 @@
-package org.iqu.persistence.service;
-
-public class Main {
-  public static void main(String[] args) {
-    NewsDAO dao = DAOFactory.getNewsDAO();
-    dao.retrieveNews(1469777576, 0, "Stiri", null, 0, null);
-  }
-}
